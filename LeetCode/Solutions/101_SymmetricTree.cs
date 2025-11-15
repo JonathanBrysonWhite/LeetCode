df394a9e0775de958f0113d03fecabf8a8f293de
J. Bryson White <jonathanbrysonwhite@gmail.com>
2025-11-15 11:17:51 -0700

    problems

---
+++ LeetCode/Solutions/101_SymmetricTree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions
+{
+    public partial class Solution
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null)
+                return true;
+            return IsSymmetricHelper(root.left, root.right);
+        }
+
+        private bool IsSymmetricHelper(TreeNode left, TreeNode right)
+        {
+            if (left == null && right == null)
+                return true;
+            if (left == null || right == null)
+                return false;
+            if (left.val != right.val)
+                return false;
+            return IsSymmetricHelper(left.left, right.right) && IsSymmetricHelper(left.right, right.left);
+        }
+    }
+}
---
+++ LeetCode/Solutions/290_WordPattern.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions
+{
+    public partial class Solution
+    {
+        public bool WordPattern(string pattern, string s)
+        {
+            Dictionary<char, string> patternMap = new Dictionary<char, string>();
+            Dictionary<string, char> stringMap = new Dictionary<string, char>();
+            string[] strings = s.Split(' ');
+
+            if (pattern.Length != strings.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                char c = pattern[i];
+                string word = strings[i];
+
+                if (!patternMap.ContainsKey(c))
+                {
+                    patternMap.Add(c, word);
+                    if (!stringMap.TryAdd(word, c))
+                    {
+                        return false;
+                    }
+                }
+                else if (!stringMap.ContainsKey(word))
+                {
+                    return false;
+                }
+                else if (patternMap[c] != word || stringMap[word] != c)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
